96832599e4ee22346108d02f6282f0795635efe6
정지우 <98355440+erase-jeong@users.noreply.github.com>
2025-12-12 15:46:09 +0900

    Time: 676 ms (13.3%), Space: 0B (100%) - LeetHub

--- 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
+++ 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
@@ -1,27 +1,9 @@
 SELECT 
-    DATE_FORMAT(trans_date, '%Y-%m') as month, 
-    country, 
-    count(*) as trans_count, 
-    sum(CASE WHEN state='approved' THEN 1 ELSE 0 END) as approved_count,
-    -- sum(state='approved') AS approved_count2,
-    sum(amount) as trans_total_amount,
-    sum(CASE WHEN state='approved' THEN amount ELSE 0 END ) as approved_total_amount
+    LEFT(trans_date, 7) AS month,
+    country,
+    COUNT(id) AS trans_count,
+    SUM(state='approved') AS approved_count,
+    SUM(amount) AS trans_total_amount,
+    SUM((state='approved')*amount) AS approved_total_amount
 FROM Transactions
-GROUP BY DATE_FORMAT(trans_date,'%Y-%m'), country
-
-/*
--- 이게 왜 가능한건지 체크하기
-SELECT *  -- DATE_FORMAT(trans_date,'%Y-%m') as qq
-FROM Transactions
-GROUP BY DATE_FORMAT(trans_date,'%Y-%m')
-*/
-
-
--- GROUP BY country
-
-
--- GROUP BY DATE_FORMAT(trans_date, '%Y-%m')
-
-/*
-각 월별 국가별 - 거래 건수, 총액, 승인된 거래 건수, 총액
-*/
+GROUP BY LEFT(trans_date,7), country;
