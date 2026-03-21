05c64b1a1c651b12a108d3ad1c5a377578449e9d
정지우 <98355440+erase-jeong@users.noreply.github.com>
2026-03-21 21:50:14 +0900

    Time: 909 ms (26.55%), Space: 0B (100%) - LeetHub

--- 1407-top-travellers/1407-top-travellers.sql
+++ 1407-top-travellers/1407-top-travellers.sql
@@ -1,7 +1,6 @@
-SELECT name, IFNULL(distance,0) as travelled_distance
-FROM Users U LEFT JOIN 
-    (SELECT user_id, SUM(distance) as distance
-    FROM Rides
-    GROUP BY user_id) AS R
-    ON U.id=R.user_id
-ORDER BY travelled_distance DESC, name ASC
+SELECT u.name, IFNULL(sum(distance),0) AS travelled_distance
+FROM Users u
+    LEFT JOIN Rides r
+ON u.id=r.user_id
+GROUP BY u.id
+ORDER BY 2 DESC, 1 ASC
