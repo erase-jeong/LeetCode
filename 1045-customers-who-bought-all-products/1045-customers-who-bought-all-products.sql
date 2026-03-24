fd9a821fb1bb180179e0547e14bc02ac607580bc
정지우 <98355440+erase-jeong@users.noreply.github.com>
2026-03-24 21:02:31 +0900

    Time: 590 ms (31.21%), Space: 0B (100%) - LeetHub

---
+++ 1045-customers-who-bought-all-products/1045-customers-who-bought-all-products.sql
@@ -0,0 +1,29 @@
+
+/*
+모든 제품을 산 사람을 찾기!!!
+=> 숫자세기 문제이다.
+1. Product테이블에 총 몇 종류의 제품이 있는지 확인
+
+2. Customer테이블에서 각 고객이 서로 다른 제품을 몇 종류 샀는지 계산함
+
+3. 고객이 구매한 제품 종류의 수가 전체 제품 종류의 수와 일치하는 사람만 골라내기
+*/
+
+/*
+SELECT count(product_key) as p_cnt
+FROM Product
+*/
+
+SELECT customer_id
+FROM 
+    (SELECT customer_id,COUNT(DISTINCT product_key) as c_cnt
+    FROM Customer
+    GROUP BY customer_id
+    HAVING c_cnt=(
+        SELECT count(product_key) as p_cnt
+        FROM Product
+        )
+    )  as sub  
+
+-- WHERE
+-- 개수로만 확인하면  되는건가?? Customer에 없는게 막 있을 수도 있고 그런거 아니야????
