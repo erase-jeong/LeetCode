6f3c232212a754488d8d147f34482be23abaef4b
정지우 <98355440+erase-jeong@users.noreply.github.com>
2026-05-06 20:41:49 +0900

    Time: 409 ms (11.58%), Space: 0B (100%) - LeetHub

--- 0626-exchange-seats/0626-exchange-seats.sql
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -1,9 +1,9 @@
 SELECT 
-    CASE
+    CASE 
         WHEN id%2=1 AND id+1<=(SELECT MAX(id) FROM Seat) THEN id+1
         WHEN id%2=0 THEN id-1
         ELSE id
     END AS id,
-    student
+    student 
 FROM Seat
-ORDER BY id;
+ORDER BY id
