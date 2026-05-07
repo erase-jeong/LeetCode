792a0a8e087c3508e681f3b635fb07f351cfcfaa
정지우 <98355440+erase-jeong@users.noreply.github.com>
2026-05-07 12:26:06 +0900

    Time: 1551 ms (12.93%), Space: 0B (100%) - LeetHub

--- 1341-movie-rating/1341-movie-rating.sql
+++ 1341-movie-rating/1341-movie-rating.sql
@@ -1,13 +1,20 @@
-(SELECT name AS results
+(
+SELECT name AS results
 FROM MovieRating JOIN Users USING(user_id)
 GROUP BY name
 ORDER BY COUNT(*) DESC, name
-LIMIT 1)
+LIMIT 1
+)
 UNION ALL
-(SELECT title AS results
-FROM MovieRating JOIN MOVIES USING(movie_id)
-WHERE EXTRACT(YEAR_MONTH FROM created_at)=202002
-GROUP BY title
+(
+SELECT title
+FROM MovieRating  mr JOIN Movies  m 
+    ON mr.movie_id=m.movie_id
+WHERE DATE_FORMAT(created_at, "%Y-%m")='2020-02'
+GROUP BY m.movie_id
 ORDER BY AVG(rating) DESC, title
 LIMIT 1
 )
+
+
+-- WHERE DATE_FORMAT("YYYY-MM",2020-02")=created_at
