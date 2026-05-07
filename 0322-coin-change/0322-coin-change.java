36fee50943fa82f71c684b74760bcdf9f7e4e6df
정지우 <98355440+erase-jeong@users.noreply.github.com>
2026-05-08 04:54:41 +0900

    Time: 11 ms (96.17%), Space: 46.5 MB (48.39%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,19 +1,16 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
         int[] dp=new int[amount+1];
-        //dp[i]=Math.min(dp[i-1]+1, dp[i-5]+1, dp[i-10]+1);
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0]=0; //어차피 디폴트
 
-        for(int i=1;i<=amount;i++){
-            int min=Integer.MAX_VALUE;
-            for(int coin:coins){
-                if(i-coin>=0 && dp[i-coin]!=Integer.MAX_VALUE){ //음수 방지
-                    min=Math.min(min,dp[i-coin]+1);
+        for(int coin : coins){
+            for(int cur=coin;cur<=amount;cur++){
+                if(dp[cur-coin]!=Integer.MAX_VALUE){
+                    dp[cur]=Math.min(dp[cur],dp[cur-coin]+1);
                 }
             }
-            dp[i]=min;
         }
-
-        //return dp[amount];
         return dp[amount]==Integer.MAX_VALUE?-1:dp[amount];
     }
 }
