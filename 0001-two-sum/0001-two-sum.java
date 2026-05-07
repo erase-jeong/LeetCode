30bca5d14f2e258bdacd94dc2be8578e8e860ddf
정지우 <98355440+erase-jeong@users.noreply.github.com>
2026-05-08 01:39:58 +0900

    Time: 5 ms (46.76%), Space: 46.7 MB (92.62%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,6 +1,26 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+        //해쉬 방법
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<nums.length;i++) map.put(nums[i],i);
+
+        for(int i=0;i<nums.length;i++){
+
+            int need=target-nums[i];
+            if(map.containsKey(need) && map.get(need)!=i){
+                return new int[]{i,map.get(need)};
+            }
+        }
+        return new int[]{0,0};
+        //throw new IllegalArgumentException("No two sum solution");
+
+        
+    }
+}
+
+
         //브루투포스트
+        /*
         for(int i=0;i<nums.length;i++){
             for(int j=0;j<nums.length;j++){
                 if(i!=j && nums[i]+nums[j]==target){
@@ -9,9 +29,5 @@ class Solution {
             }
         }
         throw new IllegalArgumentException("No two sum solution");
-
-        //return new int[]{-1};
-        
-    }
-}
+        */
 
