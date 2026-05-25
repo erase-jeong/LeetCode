1a0f3645e795c364c974de93a37b23a80a3e5638
정지우 <98355440+erase-jeong@users.noreply.github.com>
2026-05-25 15:38:59 +0900

    Time: 0 ms (100%), Space: 42.8 MB (54.74%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,25 +1,18 @@
 class Solution {
     public int rob(int[] nums) {
-        int[] dp=new int[nums.length];
+        int n=nums.length;
+        if(n==1) return nums[0];
 
-        dp[0]=nums[0];
+        int prev2=nums[0];
+        int prev1=Math.max(nums[0],nums[1]);
 
-        if(nums.length==1){
-            return dp[0];
-
-        }else if(nums.length==2){
-            dp[1]=Math.max(nums[0],nums[1]);
-            return dp[1];
-
-        }else{
-            dp[1]=Math.max(nums[0],nums[1]);
-            for(int i=2;i<nums.length;i++){  //점화식
-                dp[i]=Math.max(dp[i-1],dp[i-2]+nums[i]);
-            }
-            return dp[nums.length-1];
-
+        for(int i=2;i<n;i++){
+            int curr=Math.max(prev1, prev2+nums[i]);
+            prev2=prev1;
+            prev1=curr;
         }
-       
+
+        return prev1;
     }
 }
 /*
