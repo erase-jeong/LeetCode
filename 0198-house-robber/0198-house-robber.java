f14e42e9d66ae7ed0828c56a54967d914dd85369
정지우 <98355440+erase-jeong@users.noreply.github.com>
2026-05-25 15:34:37 +0900

    Time: 0 ms (100%), Space: 43.1 MB (5.47%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,14 +1,31 @@
 class Solution {
     public int rob(int[] nums) {
-        int n=nums.length;
-        int[] dp=new int[n];
+        int[] dp=new int[nums.length];
 
         dp[0]=nums[0];
-        if(n>1) dp[1]=Math.max(nums[0],nums[1]);
 
-        for(int i=2;i<n;i++){
-            dp[i]=Math.max(dp[i-2]+nums[i],dp[i-1]);
+        if(nums.length==1){
+            return dp[0];
+
+        }else if(nums.length==2){
+            dp[1]=Math.max(nums[0],nums[1]);
+            return dp[1];
+
+        }else{
+            dp[1]=Math.max(nums[0],nums[1]);
+            for(int i=2;i<nums.length;i++){  //점화식
+                dp[i]=Math.max(dp[i-1],dp[i-2]+nums[i]);
+            }
+            return dp[nums.length-1];
+
         }
-        return dp[n-1];
+       
     }
 }
+/*
+고려사항 : 최소 길이 1
+dp점화식 만들기 
+
+예외케이스 : 1일때랑 그거보다 클때만 나누기
+
+*/
