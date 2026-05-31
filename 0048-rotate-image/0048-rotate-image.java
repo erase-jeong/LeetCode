09823f518f8de4172eb047d9386c2622c5d1a742
정지우 <98355440+erase-jeong@users.noreply.github.com>
2026-05-31 20:48:12 +0900

    Time: 10 ms (1.18%), Space: 44.3 MB (15.35%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,36 +1,28 @@
-import java.util.*;
 class Solution {
     public void rotate(int[][] matrix) {
         int n=matrix.length;
-        int[][] ans=new int[n][n];
-        
+
+        int[][] tmp=new int[n][n];
+        //1st. 행,열 바꾸기
         for(int i=0;i<n;i++){
-            //System.out.println("i : "+i);
-            for(int j=n-1;j>=0;j--){
-                //System.out.println("["+j+"]["+i+"] :"+matrix[j][i]);
-                //System.out.print(matrix[j][i]+" ");
-                ans[i][n-1-j]=matrix[j][i];
+            for(int j=0;j<n;j++){
+                tmp[i][j]=matrix[j][i];
+            }
+        }
+        //확인용 출력
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                System.out.print(tmp[i][j]);
             }
             System.out.println();
         }
 
-        System.out.println();
 
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
-                System.out.print(ans[i][j]+" ");
-                matrix[i][j]=ans[i][j];
+                matrix[i][j]=tmp[i][n-1-j];
             }
-            System.out.println();
         }
 
-        //return ans;
-
-
     }
-} 
-
-/*
-0 과 n-1  -> 두개 합쳐서 n-1
-n-1-j
-*/
+}
