61006ac98f79a6eb3b7f7d1c6587b741859aa5aa
정지우 <98355440+erase-jeong@users.noreply.github.com>
2026-06-01 13:56:33 +0900

    Time: 4 ms (11%), Space: 46.8 MB (15.38%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.java
+++ 0695-max-area-of-island/0695-max-area-of-island.java
@@ -1,47 +1,61 @@
 class Solution {
-    static int rows, cols;
+    static int[] dx={0,0,-1,1};
+    static int[] dy={-1,1,0,0};
+    static int n,m;
+    static boolean[][] visited;
 
     public int maxAreaOfIsland(int[][] grid) {
-        rows=grid.length;
-        cols=grid[0].length;
+        n=grid.length;
+        m=grid[0].length;
 
-        int maxArea=0;
+        visited=new boolean[n][m];
 
-        for(int r=0;r<rows;r++){
-            for(int c=0;c<cols;c++){
-                if(grid[r][c]==1){
-                    //섬 발견 -> DFS로 넓이 계산
-                    int area=bfs(r,c,grid);
-                    maxArea=Math.max(maxArea,area);
-                }
+        int answer=0;
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==1 && !visited[i][j]){
+                    answer=Math.max(answer,bfs(i,j,grid));
+                } 
             }
         }
-        return maxArea;
+
+        return answer;
     }
 
-    public int bfs(int r, int c, int[][] grid){
-        Queue<int[]> queue=new LinkedList<>();
-        queue.add(new int[]{r,c});
-        grid[r][c]=0;
-        int area=0;
+    public static int bfs(int x, int y, int[][] grid){
+        visited[x][y]=true;
+        int count=1;
+        Queue<int[]> q=new LinkedList<>();
+        q.add(new int[]{x,y});
 
-        int[][] dirs={{1,0},{-1,0},{0,1},{0,-1}};
-
-        while(!queue.isEmpty()){
-            int[] cur=queue.poll();
-            area++;
-
-            for(int[] d:dirs){
-                int nr=cur[0]+d[0];
-                int nc=cur[1]+d[1];
-                if(nr>=0 && nr<rows && nc>=0 && nc<cols && grid[nr][nc]==1){
-                    grid[nr][nc]=0;
-                    queue.add(new int[]{nr,nc});;
-                }
+        while(!q.isEmpty()){
+            int[] p=q.poll();
+            for(int d=0;d<4;d++){
+                int nx=p[0]+dx[d];
+                int ny=p[1]+dy[d];
+                //이 중에 뭐 하나 걸리면 낙오임
+                if(nx<0 || nx>=n || ny<0 || ny>=m || visited[nx][ny]) continue;
+                if(grid[nx][ny]==1){
+                    q.add(new int[]{nx,ny});
+                    visited[nx][ny]=true;
+                    count++;
+                }                
             }
         }
-        return area;
+
+        return count;
     }
-
-
 }
+
+
+/*
+상하좌우 움직이기
+
+덩어리들 찾아내기 => 그 중에 제일 큰걸로 출력하기
+==> 계속 비교하면서 갱신로직 만들기
+
+dfs, bfs 뭘로 풀어야 할까? 차이는 뭘까?
+bfs로 풀꺼면 Queue를 통해서 상하좌우 이동하면서 넣을꺼 있는지 확인하고
+하는로직으로 풀기. 해야한다.
+*/
