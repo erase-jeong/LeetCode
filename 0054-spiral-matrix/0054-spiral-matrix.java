7f2634e00d39a0b8046377144ad0428df57162e0
정지우 <98355440+erase-jeong@users.noreply.github.com>
2026-06-01 11:22:07 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -1,34 +1,11 @@
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
-        //오른쪽, 아래, 왼쪽, 위쪽
+        //정답
+        List<Integer> ans=new ArrayList<>();
 
-        int[] dx={0,1,0,-1}; 
-        int[] dy={1,0,-1,0};
+        int m=matrix.length;
+        int n=matrix[0].length;
 
-        int n=matrix.length;
-        int m=matrix[0].length;
-        boolean[][] visited=new boolean[n][m];
-        List<Integer> answer=new ArrayList<>();
-        int idx=0;
-        int x=0, y=0;
-
-        while(true){
-            if(answer.size()==n*m) break;
-
-            if(x<0||x>=n||y<0||y>=m||visited[x][y]){
-                x-=dx[idx];
-                y-=dy[idx];
-                idx=(idx+1)%4;
-            }
-
-            if(!visited[x][y]){
-                answer.add(matrix[x][y]);
-                visited[x][y]=true;
-            }
-
-            x+=dx[idx];
-            y+=dy[idx]; 
-        }
-        return answer;
+        
     }
 }
