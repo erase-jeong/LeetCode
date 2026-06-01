db9ae8141903e795e2fdb211572f58d3392208bd
정지우 <98355440+erase-jeong@users.noreply.github.com>
2026-06-01 11:08:27 +0900

    Time: 0 ms (100%), Space: 42.8 MB (88.8%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -1,26 +1,33 @@
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
-        int[] dx = {0, 1, 0, -1};
-        int[] dy = {1, 0, -1, 0};
-        int n = matrix.length;
-        int m = matrix[0].length;
-        boolean[][] visited = new boolean[n][m];
-        List<Integer> answer = new ArrayList<>();
-        int idx = 0;
-        int x = 0, y = 0;
-        while (true) {
-            if (answer.size() == n * m) break;
-            if (x < 0 || x >= n || y < 0 || y >= m || visited[x][y]) {
-                x -= dx[idx];
-                y -= dy[idx];
-                idx = (idx + 1) % 4;
+        //오른쪽, 아래, 왼쪽, 위쪽
+
+        int[] dx={0,1,0,-1}; 
+        int[] dy={1,0,-1,0};
+
+        int n=matrix.length;
+        int m=matrix[0].length;
+        boolean[][] visited=new boolean[n][m];
+        List<Integer> answer=new ArrayList<>();
+        int idx=0;
+        int x=0, y=0;
+
+        while(true){
+            if(answer.size()==n*m) break;
+
+            if(x<0||x>=n||y<0||y>=m||visited[x][y]){
+                x-=dx[idx];
+                y-=dy[idx];
+                idx=(idx+1)%4;
             }
-            if (!visited[x][y]) {
+
+            if(!visited[x][y]){
                 answer.add(matrix[x][y]);
-                visited[x][y] = true;
+                visited[x][y]=true;
             }
-            x += dx[idx];
-            y += dy[idx];
+
+            x+=dx[idx];
+            y+=dy[idx]; 
         }
         return answer;
     }
