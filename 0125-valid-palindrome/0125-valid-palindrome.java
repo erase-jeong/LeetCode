4fe97707a70ee1581788d0d5cacdf3545ce9684d
정지우 <98355440+erase-jeong@users.noreply.github.com>
2026-06-01 14:45:02 +0900

    Time: 17 ms (11.39%), Space: 46.3 MB (31.46%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,16 +1,22 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        s=s.toLowerCase();       //소문자로 변환
-        s=s.replaceAll("[^a-z0-9]","");   //영어, 숫자 제외하고 제거
-
-        int left=0;
-        int right=s.length()-1;
         
-        while(left<=right){
-            if(s.charAt(left)!=s.charAt(right)) return false;
-            left++;
-            right--;
+
+        s=s.toLowerCase();
+        s=s.replaceAll("[^a-z0-9]","");
+        System.out.println(s);
+        int n=s.length();
+        boolean flag=true;
+        for(int i=0;i<n/2;i++){
+            if(s.charAt(i)!=s.charAt(n-1-i)) flag=false; 
         }
-        return true;
+        return flag;
     }
 }
+
+/*
+자바 String에서 
+1. 대문자 -> 소문자로 바꾸기
+2. 영문자 아닌 모든 문제 제거하기
+
+*/
