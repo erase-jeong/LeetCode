4a16a325cae03898141794583f01410c93acf50d
정지우 <98355440+erase-jeong@users.noreply.github.com>
2026-06-01 13:22:26 +0900

    Time: 0 ms (100%), Space: 42.6 MB (98.27%) - LeetHub

--- 0289-game-of-life/0289-game-of-life.java
+++ 0289-game-of-life/0289-game-of-life.java
@@ -1,82 +1,50 @@
 class Solution {
-    static int[] dr={1,-1,0,0,   1,-1,1,-1};
-    static int[] dc={0,0,1,-1,   1,1,-1,-1};
-
-    static int m;
+    //상하좌우, 대각선
+    static int[] dx={0,0,-1,1, -1,-1,1,1};
+    static int[] dy={-1,1,0,0,-1,1,-1,1};
     static int n;
+    static int m;
 
     public void gameOfLife(int[][] board) {
-        n=board.length; //세로(행)
-        m=board[0].length; //가로(열)
+        n=board.length;
+        m=board[0].length;
 
-        int[][] temp=new int[n][m];
+        int[][] tmp=new int[n][m];
 
-        //board -> temp
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(board[i][j]==1){
-                    //살아있는 세포 2개 미만
-                    if(LiveCnt(i,j,board)<2) temp[i][j]=0; 
-
-                    //살아있는 세포 2~3개
-                    else if(LiveCnt(i,j,board)==2 || LiveCnt(i,j,board)==3) temp[i][j]=1;
-                    
-                    //살아있는 세포 3개 이상
-                    else if(LiveCnt(i,j,board)>3) temp[i][j]=0;
-                
-                }else if(board[i][j]==0){
-                    //죽은세포=> 살아있는 세포 3개 인접
-                    //(Live)
-                    if(LiveCnt(i,j,board)==3) temp[i][j]=1;
+                    if(nearBy(i,j,board)<2) tmp[i][j]=0;
+                    else if(nearBy(i,j,board)==2 || nearBy(i,j,board)==3) tmp[i][j]=1;
+                    else if(nearBy(i,j,board)>3) tmp[i][j]=0;
+                    else tmp[i][j]=tmp[i][j];
+                }else{
+                    if(nearBy(i,j,board)==3) tmp[i][j]=1;
+                    else tmp[i][j]=tmp[i][j];
                 }
-
-       
             }
         }
 
-        //temp -> board
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                board[i][j]=temp[i][j];
+                board[i][j]=tmp[i][j];
             }
         }
-            
 
-
-
-
-        
     }
 
-    public static int LiveCnt(int r, int c, int[][] board){
-        int cnt=0; 
+    public static int nearBy(int x,int y,int[][] board){
+        //현재위치(x,y)를 기준으로 8방향 탐색해서 몇개가 1인지 찾는거
+        //근데 이거 동시에 되는거니깐 한번 넣고 복사해서 다시 넣어야 할듯?
+        int count=0;
+
         for(int d=0;d<8;d++){
-            int nr=r+dr[d];
-            int nc=c+dc[d];
-            if(0<=nr && nr<n && 0<=nc && nc<m){
-                if(board[nr][nc]==1) cnt++;
-            } 
-            
+            int nx=x+dx[d];
+            int ny=y+dy[d];
+            if(nx<0 || nx>=n || ny<0 || ny>=m) continue;
+            if(board[nx][ny]==1) count++; //이거 왜 안되는거지?
         }
-        return cnt;
+
+        return count;
     }
-
-    /*
-    public static int DeadCnt(int r, int c, int[][] board){
-        int cnt=0;
-
-
-
-        return cnt; 
-    }
-    */
 }
-
-/*
-생존 : 1
-사망 : 0
-
-주변에 살아있는 세포가 2개 미만 -> 개체수 부족 => 죽은 것처럼 보임
-
-살아있는 세포 3개 이상 => 죽은것처럼 보인
-*/
