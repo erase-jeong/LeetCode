282056048c4fb461c4b085381cc6d7e73d9759e8
정지우 <98355440+erase-jeong@users.noreply.github.com>
2026-06-02 19:44:43 +0900

    Time: 3 ms (5.8%), Space: 94.3 MB (82.66%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,24 +1,14 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int minPrice=prices[0];  //지금까지 본 가격 중 최솟값
-        int maxProfit=Integer.MIN_VALUE; //지금까지 구한 이윤 중 최댓값
+        int answer=0;
 
-        for(int i=0;i<prices.length;i++){
-            int curPrice=prices[i]; //현재 가격
-            if(curPrice<minPrice){
-                minPrice=prices[i];
-            }
-            if(curPrice-minPrice>maxProfit) maxProfit=curPrice-minPrice;
+        int min=prices[0];
+        for(int i=1;i<prices.length;i++){
+            min=Math.min(min,prices[i-1]);
+            int profit=prices[i]-min;
+            answer=Math.max(answer,profit);
         }
 
-        System.out.println(maxProfit);
-        return maxProfit;
-        
+        return answer;
     }
 }
-
-/*
-언제 사서 언제 파는게 제일 좋을까?
-그리디. 
-오늘샀을 때, 내일 파는거 다음에 파는 
-*/
