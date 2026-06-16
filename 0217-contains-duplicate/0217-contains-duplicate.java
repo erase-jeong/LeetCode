8bd938daa11d079a37de4d29b24a2794f803d094
정지우 <98355440+erase-jeong@users.noreply.github.com>
2026-06-16 20:49:52 +0900

    Time: 25 ms (11.92%), Space: 80.7 MB (79.39%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set=Arrays.stream(nums)
+                            .boxed()
+                            .collect(Collectors.toSet());
+
+        return set.size()!=nums.length;
+    }
+}
