74af95b5cb992f1b19c3bd8d3f92cab46acf0314
정지우 <98355440+erase-jeong@users.noreply.github.com>
2026-06-26 20:24:42 +0900

    Time: 617 ms (18.89%), Space: 0B (100%) - LeetHub

---
+++ 1795-rearrange-products-table/1795-rearrange-products-table.sql
@@ -0,0 +1,18 @@
+-- store1의 데이터를 행으로 조회
+SELECT product_id, 'store1' AS store, store1 AS price
+FROM Products
+WHERE store1 IS NOT NULL
+
+UNION ALL 
+
+-- store2의 데이터를 행으로 조회
+SELECT product_id, 'store2' AS store, store2 AS price
+FROM Products
+WHERE store2 IS NOT NULL
+
+UNION ALL
+
+-- store3의 데이터를 행으로 조회 (이 부분을 수정합니다)
+SELECT product_id, 'store3' AS store, store3 AS price
+FROM Products
+WHERE store3 IS NOT NULL;
