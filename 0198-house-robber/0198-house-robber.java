d54b535dc691219519ca7097f842e63b1931024e
정지우 <98355440+erase-jeong@users.noreply.github.com>
2026-07-27 21:41:49 +0900

    Time: 3 ms (0.53%), Space: 42.8 MB (57.28%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,15 +1,37 @@
+import java.util.*; 
+
 class Solution {
     public int rob(int[] nums) {
         if(nums.length==1) return nums[0];
-
+        if(nums.length==2) return Math.max(nums[0], nums[1]);
+        
         int[] dp=new int[nums.length];
-        dp[0]=nums[0];
-        dp[1]=Math.max(dp[0],nums[1]);
+        //Arrays.fill(nums,1);
 
-        for(int i=2;i<nums.length;i++){
-            dp[i]=Math.max(dp[i-1],dp[i-2]+nums[i]); 
+        //System.out.println(Arrays.toString(nums));
+        dp[0]=nums[0];
+        dp[1]=nums[1];
+        //dp[2]=nums[2]+Math.max(dp[0],dp[1]);
+        dp[2]=nums[2]+dp[0];
+
+        for(int i=3;i<nums.length;i++){
+            //System.out.println("");
+            dp[i]=nums[i]+Math.max(dp[i-2],dp[i-3]);
         }
+        //System.out.println(Arrays.toString(dp));
+        Arrays.sort(dp);
 
         return dp[nums.length-1];
     }
 }
+
+
+/*
+[1, 2, 3, 1]
+[1, 2, 4, 3]
+dp[0]=nums[0];
+dp[1]=nums[1];
+
+dp[2]=nums[2]+dp[0];
+
+*/
