f7d0e90e5f81fcb0053dabfe750f990271b6bf4e
정지우 <98355440+erase-jeong@users.noreply.github.com>
2026-07-28 00:39:56 +0900

    Time: 792 ms (5.18%), Space: 123.6 MB (5.02%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -2,10 +2,29 @@ import java.util.*;
 
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        Set<Integer> set=Arrays.stream(nums)
-                            .boxed()
-                            .collect(Collectors.toSet());
+        Map<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        }
+        System.out.println(map);
 
-        return set.size()!=nums.length;
+        boolean answer=false;
+
+        for(int key : map.keySet()){
+            System.out.println(key+" : "+map.get(key));
+            if(map.get(key)>1) return true;
+        }
+
+        return false;
     }
 }
+
+/*
+set을 이용해서 현재 몇개의 원소들이 있는지 파악하기
+여기에 맞게 
+
+아니다. hashmap을 만들어서 key-value 만들기
+그 다음에 어떻게든 하기
+
+
+*/
