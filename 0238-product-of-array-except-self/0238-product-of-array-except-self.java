0a2c31741a5f638d4ef13634494659e5107f9704
정지우 <98355440+erase-jeong@users.noreply.github.com>
2026-07-31 20:54:42 +0900

    Time: 4 ms (6.08%), Space: 65 MB (81.07%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,19 +1,24 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
         int n=nums.length;
+        int[] left=new int[n];
+        int[] right=new int[n];
         int[] answer=new int[n];
 
-        //1단계 : 왼쪽 누적곱(i왼쪽 원소들의 곱)
-        answer[0]=1;
+        //1단계 : 왼쪽 누적 곱
+        left[0]=1;
         for(int i=1;i<n;i++){
-            answer[i]=answer[i-1]*nums[i-1];
+            left[i]=left[i-1]*nums[i-1];
         }
 
-        //2단계 : 오른쪽 누적곱을 변수 하나로 흘려보내며 곱하기
-        int right=1;
-        for(int i=n-1;i>=0;i--){
-            answer[i]*=right; //왼쪽 곱 * 오른쪽 곱
-            right*=nums[i];
+        //2단계 : 오른쪽 누적 곱
+        right[n-1]=1;
+        for(int i=n-2;i>=0;i--){
+            right[i]=right[i+1]*nums[i+1];
+        }
+
+        for(int i=0;i<n;i++){
+            answer[i]=left[i]*right[i];
         }
         return answer;
     }
