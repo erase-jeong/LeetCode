bb39285947d667210371084ae21241a026f12ae0
정지우 <98355440+erase-jeong@users.noreply.github.com>
2026-07-31 20:19:00 +0900

    Time: 2 ms (93.93%), Space: 72.4 MB (6.57%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,42 +1,20 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int mul=1;
-        for(int num : nums){
-            mul*=num;
+        int n=nums.length;
+        int[] answer=new int[n];
+
+        //1단계 : 왼쪽 누적곱(i왼쪽 원소들의 곱)
+        answer[0]=1;
+        for(int i=1;i<n;i++){
+            answer[i]=answer[i-1]*nums[i-1];
         }
 
-        int[] answer=new int[nums.length];
-        List<Integer> lst=new ArrayList<>();
-
-        for(int i=0;i<answer.length;i++){
-            if(nums[i]==0){
-                //answer[i]=mul;
-                //answer[i]=mul
-                //예외적으로 따로 처리를 해야한다. 어떻게?
-                //인덱스만 따로 모아두고, 나중에 예외처리를 진행한다.
-                int mul2=1;
-                for(int j=0;j<answer.length;j++){
-                    if(j==i) continue;
-                    else mul2*=nums[j];
-                }
-                System.out.println("i : "+i);
-                System.out.println(mul2);
-                answer[i]=mul2;
-                //lst.add(i);
-
-            }else{
-                answer[i]=mul/nums[i];
-            }
-
+        //2단계 : 오른쪽 누적곱을 변수 하나로 흘려보내며 곱하기
+        int right=1;
+        for(int i=n-1;i>=0;i--){
+            answer[i]*=right; //왼쪽 곱 * 오른쪽 곱
+            right*=nums[i];
         }
         return answer;
     }
 }
-
-
-/*
-전체 파악
-자신 제외하고 곱하기
-전체곱을 미리 구해두고, 자기자신으로 나누기
-그럼for() 2번
-*/
