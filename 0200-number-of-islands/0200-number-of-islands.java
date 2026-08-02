b7f2cb5deced653ae061b503824b4e812b26602c
정지우 <98355440+erase-jeong@users.noreply.github.com>
2026-08-02 16:00:41 +0900

    Time: 6 ms (20.19%), Space: 51.1 MB (97.61%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,75 +1,51 @@
 class Solution {
-    static int[][] visited;
     static int[] dr={-1,1,0,0};
     static int[] dc={0,0,-1,1};
-    
+    static boolean[][] visited;
 
     public int numIslands(char[][] grid) {
-        int n=grid.length;
-        int m=grid[0].length;
-
-        //System.out.println("n : "+n+", m : "+m);
-
-        visited=new int[n][m];
-        //System.out.println("visited : "+Arrays.deepToString(visited));
-
+        visited=new boolean[grid.length][grid[0].length];
         int answer=0;
 
         for(int i=0;i<grid.length;i++){
             for(int j=0;j<grid[0].length;j++){
-                if(grid[i][j]=='1'&& visited[i][j]==0){
-                    System.out.println("i : "+i+", j : "+j);
-                    //System.out.println("bfs 진입 : i="+i+", j="+j);
-                    //bfs(i,j, grid); 
-                    //answer++;
-                    answer+=bfs(i,j,grid);
-                } 
+                //bfs
+                if(grid[i][j]=='1' && visited[i][j]==false){
+                    answer+=bfs(i,j, grid);
+                }
             }
-            //System.out.println();
         }
+
         return answer;
     }
-    
-    public static int bfs(int i, int j, char[][] grid){  //bfs 1번에 하나의 덩어리를 찾아낸다
+
+    public int bfs(int r, int c, char[][] grid){
+
         Queue<int[]> queue=new LinkedList<>();
-        queue.add(new int[]{i,j});
-        visited[i][j]=1;
-
-
-        //System.out.println("초기 queue : " + Arrays.deepToString(queue.toArray()));
+        queue.add(new int[]{r,c});
+        visited[r][c]=true;
 
         while(!queue.isEmpty()){
             int[] p=queue.poll();
-            //System.out.println("p : "+Arrays.toString(p));
-
+            
             for(int d=0;d<4;d++){
                 int nr=p[0]+dr[d];
                 int nc=p[1]+dc[d];
 
-                //System.out.println("nr : "+nr+", nc : "+nc);
-
-                if(nr>=0 && nr<grid.length && nc>=0 && nc<grid[0].length && grid[nr][nc]=='1' && visited[nr][nc]==0){
-                    //System.out.println("if문 내부 - nr : "+nr+", nc : "+nc);
-                  //queue.add(new int{nr,nc});
-                    visited[nr][nc]=1;
-                    //System.out.println("visited["+nr+"]["+nc+"] :"+visited[nr][nc]);
-
+                if(nr>=0 && nr<grid.length && nc>=0 && nc<grid[0].length 
+                && grid[nr][nc]=='1' && visited[nr][nc]==false){
+                    visited[nr][nc]=true;
                     queue.add(new int[]{nr,nc});
                 }
             }
+
         }
-        
         return 1;
     }
-
-
 }
 
-/*
-bfs로 퍼트리면서 주변을 퍼트리면서 확인하는 로직
 
-하는 방법
-1. 방문 배열(2차원)
-2. 방문하지 않았거나 1이며 방문
-전체 로직 설계
+/*
+전형적인 bfs문제이다.
+상하좌우 퍼트려가면서, 같은 덩어리를 찾는 문제이다.
 */
