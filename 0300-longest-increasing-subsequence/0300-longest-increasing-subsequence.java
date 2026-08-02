c01eecb908eb7f07864d26627b1046a1d0b7ab7e
정지우 <98355440+erase-jeong@users.noreply.github.com>
2026-08-02 16:53:29 +0900

    Time: 52 ms (30.91%), Space: 47.1 MB (23.89%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -3,11 +3,14 @@ class Solution {
         int[] dp=new int[nums.length];
         int[] maxV=new int[nums.length];
 
+        /*
         for(int i=0;i<dp.length;i++){
             dp[i]=1;
         }
+        */
 
-        //Arrays.fill(dp, 1);
+        Arrays.fill(dp, 1);
+        
         for(int i=1;i<dp.length;i++){
             for(int j=0;j<i;j++){
                 if(nums[i]>nums[j]) dp[i]=Math.max(dp[j]+1,dp[i]);
